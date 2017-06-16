6c7f4730c11ef41c71d5644989d7d2f04fd01ebe
Hokuto Shinoda <pinkumohikan+github@gmail.com>
2017-06-16 14:01:43 +0900

    refactor(file_path): nullを最後に弾くスタイルにして短くしてみた

--- r/file_path.r
+++ r/file_path.r
@@ -2,12 +2,9 @@
 get_path <- function() {
     paths <- c()
 
-    for (fn in 1:sys.nframe()) {
-        path <- sys.frame(fn)$ofile
-        if (!is.null(path)) {
-            paths <- append(paths, path)
-        }
+    for (fn in 0:(sys.nframe() - 1)) {
+        paths <- append(paths, sys.frame(fn)$ofile)
     }
 
-    return(tail(paths, n=1))
+    return(tail(na.omit(paths), n=1))
 }
