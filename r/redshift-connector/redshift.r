34a1c6f572774ddd47ac90d73b914351d1585400
Hokuto Shinoda <pinkumohikan+github@gmail.com>
2017-07-01 04:58:44 +0900

    fix(r): ディレクトリが間違っていた

--- r/redshift-connector/redshift.r
+++ r/redshift-connector/redshift.r
@@ -7,7 +7,7 @@ connectRedshift <- function() {
     #   * http://docs.aws.amazon.com/ja_jp/redshift/latest/mgmt/connecting-ssl-support.html#connect-using-ssl
     #   * http://docs.aws.amazon.com/ja_jp/redshift/latest/mgmt/configure-jdbc-options.html
 
-    driver <- JDBC('com.amazon.redshift.jdbc.Driver', '/RedshiftJDBC42-1.2.1.1001.jar', identifier.quote="`")
+    driver <- JDBC('com.amazon.redshift.jdbc.Driver', './redshift/RedshiftJDBC42-1.2.1.1001.jar', identifier.quote="`")
     url <- sprintf(
         'jdbc:redshift://%s:%s/%s?AuthMech=REQUIRE&ssl=true&sslMode=verify-ca&sslrootcert=%s&user=%s&password=%s',
         Sys.getenv('REDSHIFT_HOST'),
