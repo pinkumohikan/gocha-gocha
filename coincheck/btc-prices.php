b0fa35cfc30ef29f0ab6f7ea0f5c837ca8b369e0
Hokuto Shinoda <pinkumohikan+github@gmail.com>
2017-12-31 05:50:24 +0900

    feat(coincheck): 価格履歴を取得して整形

---
+++ coincheck/btc-prices.php
@@ -0,0 +1,18 @@
+<?php
+
+ini_set('date.timezone', 'Asia/Tokyo');
+
+const END_POINT = 'https://coincheck.com/api/charts/candle_rates?limit=300&market=coincheck&pair=btc_jpy&unit=3600&v2=true';
+
+$prices = json_decode(file_get_contents(END_POINT), true);
+$formatted = array_map(function(array $raw) {
+    return [
+        'time'    => date('Y-m-d H:i:s', $raw[0]),
+        'start'   => $raw[1],
+        'highest' => $raw[2],
+        'lowest'  => $raw[3],
+        'finish'  => $raw[4],
+    ];
+}, $prices);
+
+var_dump($formatted);
