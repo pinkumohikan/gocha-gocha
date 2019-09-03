6520e69bc87576acc4572d21dc25adcf55f6f4e7
Hokuto Shinoda <pinkumohikan+github@gmail.com>
2019-09-03 18:16:25 +0900

    feat: BatchRequestする版を書いてみた

---
+++ scala/sqs-enqueue-bench/src/main/scala/example/SendMessageBatch.scala
@@ -0,0 +1,29 @@
+package example
+
+import java.time.ZonedDateTime
+
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.sqs.SqsClient
+import software.amazon.awssdk.services.sqs.model.{SendMessageBatchRequest, SendMessageBatchRequestEntry}
+
+object SendMessageBatch extends App {
+  println("started")
+  val start = ZonedDateTime.now()
+
+  val sqsClient = SqsClient.builder().region(Region.AP_NORTHEAST_1).build()
+  val queueUrl = sys.env("QUEUE_URL")
+  val count = sys.env("COUNT").toInt
+
+  (1 to count)
+    .map(id => SendMessageBatchRequestEntry.builder().id(s"$id").messageBody(s"id=$id").build())
+    .grouped(10)
+    .toList
+    .map(messages => SendMessageBatchRequest.builder().queueUrl(queueUrl).entries(messages: _*).build())
+    .map(req => {
+      println("calling sendMessageBatch")
+      sqsClient.sendMessageBatch(req)
+    })
+
+  println("finished")
+  println(s"elapsed: ${ZonedDateTime.now().toEpochSecond - start.toEpochSecond} sec")
+}
