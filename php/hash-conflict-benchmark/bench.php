db940730184291aaa552d43faf77f0945b9688a8
Hokuto Shinoda <pinkumohikan+github@gmail.com>
2019-11-22 15:30:51 +0900

    feat: メモリ使用量もきになる

--- php/hash-conflict-benchmark/bench.php
+++ php/hash-conflict-benchmark/bench.php
@@ -7,6 +7,8 @@ function main(array $argv): void
 
     var_dump('count($hashs):', count($hashs));
     var_dump('count(array_unique($hashs)):', count(array_unique($hashs)));
+
+    var_dump('memory_get_peak_usage(MB):', memory_get_peak_usage(true) / 1024 / 1024);
 }
 
 function generateHashs(int $max): array
